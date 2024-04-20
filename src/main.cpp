38e8cfd9fbbf0b1a877257e43d894abe99dc3825
andreassisrg <andreassisrg@gmail.com>
2024-04-20 12:21:32 -0300

    Esqueleto Classe Database

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,43 @@
 
 class Database
 {
+	private:
+		sqlite3* db;
+		// Construtor para prevenção da instanciação da db
+		Database() {
+        int rc = sqlite3_open("database.sqlite", &db);
+        if (rc != SQLITE_OK) {
+            std::cerr << "Cannot open database: " << sqlite3_errmsg(db) << std::endl;
+            sqlite3_close(db);
+            exit(1);
+        }
+    }
 
+	public:
+		// Função para que outros módulos acessem a db
+		static Database& getInstancia()
+		{
+			static Database instance;
+			return instance;
+    	}
+
+		// Método para facilitar a execução de queries
+		void query(const std::string& sql_query) {
+			char* errMsg;
+			int rc = sqlite3_exec(db, sql_query.c_str(), nullptr, nullptr, &errMsg);
+			if (rc != SQLITE_OK) {
+				std::cerr << "SQL error: " << errMsg << std::endl;
+				sqlite3_free(errMsg);
+			} else {
+				std::cout << "Query executed successfully" << std::endl;
+			}
+		}
+
+	// Desconstrutor para não crashar a db
+	~Database()
+	{
+		sqlite3_close(db);
+    }
 };
 
 int main()
