e886c43e925d0ff6d1abef7723c784356d25c02a
andreassisrg <andreassisrg@gmail.com>
2024-04-21 09:55:51 -0300

    Main Atualizada

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,20 @@
 
 int main()
 {
+	/*
+		Bem Vindo ao Banquin!
+
+		Implemenetação de dois menus, um para gerar autenticação do usuário e outro para
+		prover as opções dadas ao usuário. Utilização de uma variável para armazenar a
+		intenção do usuário.
+	*/
+
 	int opcao {};
 
 	// Primeiro Menu - Acesso ao Usuário
 	while(true)
 	{
-		std::cout << "Bem Vindo ao Banquinho!" << '\n';
+		std::cout << "Bem Vindo ao Banquin!" << '\n';
 		std::cout << "Primeiro, registre-se ou entre em nosso sistema." << '\n';
 		std::cout << "[1] Entrar na Minha conta" << '\n';
 		std::cout << "[2] Criar Conta" << '\n';
@@ -28,9 +36,7 @@ int main()
 		}
 	}
 
-	/* TO DO
-		Registro do usuário!
-	*/
+	// TO DO - Registro do usuário
 
 	// Segundo Menu - Opções do Usuário
 	while(true)
@@ -38,13 +44,20 @@ int main()
 		opcao = 0;
 
 		std::cout << "Bem Vindo <usuario>, o que voce gostaria de fazer hoje?" << '\n';
-		std::cout << "[1] ..." << '\n';
-		std::cout << "[10] Sair" << '\n';
+		std::cout << "[1] Depositar" << '\n';
+		std::cout << "[2] Sacar" << '\n';
+		std::cout << "[3] Transferir" << '\n';
+		std::cout << "[4] Comprar Ações" << '\n';
+		std::cout << "[5] Comprar Criptomoedas" << '\n';
+		std::cout << "[6] Visualizar meu Extrato" << '\n';
+		std::cout << "[7] Sair" << '\n';
 
-		if (opcao == 10)
+		std::cin >> opcao;
+
+		if (opcao == 7)
 		{
 			std::cout << "Tchau <usuario>!!!" << std::endl;
+			exit(0);
 		}
-
 	}
 }
