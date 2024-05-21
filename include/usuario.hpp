b0831b6ada75d34e53e3e2e3126886a7633649ca
andreassisrg <andreassisrg@gmail.com>
2024-05-21 13:16:39 -0300

    indentação

--- include/usuario.hpp
+++ include/usuario.hpp
@@ -5,18 +5,18 @@
 
 class Usuario
 {
-private:
-    std::string nome;
-    int idUsuario;
+    private:
+        std::string nome;
+        int idUsuario;
 
-public:
-    Usuario(); // Construtor padrão
-    Usuario(const std::string &nome, int idUsuario); // Construtor com parâmetros
-    void criarConta(const std::string &nomeNovoUsuario); // Função para criar uma nova conta de usuário
-    void logIn(); // Função para realizar o login do usuário
-    void logOut(); // Função para realizar o logout do usuário
-    std::string getNome(); // Getter para o nome do usuário
-    int getId(); // Getter para o ID do usuário
+    public:
+        Usuario(); // Construtor padrão
+        Usuario(const std::string &nome, int idUsuario); // Construtor com parâmetros
+        void criarConta(const std::string &nomeNovoUsuario); // Função para criar uma nova conta de usuário
+        void logIn(); // Função para realizar o login do usuário
+        void logOut(); // Função para realizar o logout do usuário
+        std::string getNome(); // Getter para o nome do usuário
+        int getId(); // Getter para o ID do usuário
 };
 
 #endif
