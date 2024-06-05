d22c1f4a15257bc92f7fed0e9f07fcb547138453
andreassisrg <andreassisrg@gmail.com>
2024-06-05 20:15:19 -0300

    feature: construtor e funcionalidade inicial Banco

--- src/banco.cpp
+++ src/banco.cpp
@@ -1,3 +1,26 @@
 #include "../include/banco.hpp"
+#include "ExcecaoCpf.hpp"
+#include "ExcecaoNegativo.hpp"
+#include "ExcecaoSaque.hpp"
+#include "ExcecaoTransferencia.hpp"
 
-//comando qualquers
+
+bool existe(std::string palavra, std::vector<std::string> vetor)
+{
+    for (unsigned int i = 0; i < vetor.size(); i++)
+    {
+        if (vetor[i] == palavra)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+Banco::Banco()
+{
+    std::cout << "\n\n    ============ Seja bem-vindo ao NossoBanco! ================" << std::endl;
+    std::cout << "====================================================================\n";
+    saldo = 10000;
+}
