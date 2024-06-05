2c40e421973e9969773cd62af79cbe256d0aaae3
andreassisrg <andreassisrg@gmail.com>
2024-06-05 20:16:08 -0300

    feature: principais operações Banco

--- src/banco.cpp
+++ src/banco.cpp
@@ -24,3 +24,114 @@ Banco::Banco()
     std::cout << "====================================================================\n";
     saldo = 10000;
 }
+
+void Banco::realizaOperacao(int opcao, Conta *conta)
+{
+    if (opcao == 1)
+    {
+        float valorSaque;
+        std::cout << "====================================================================\n";
+        std::cout << "Digite o valor do saque ou digite 9 para cancelar a operação" << std::endl;
+        std::cout << "↓\n";
+        std::cin >> valorSaque;
+        system("clear");
+        if (valorSaque == 9){
+            system("clear");
+            Banco::selecionaOperacao();
+        }
+            else {
+            if (valorSaque <= saldo)
+            {
+                conta->realizaSaque(valorSaque);
+                Transacao t(valorSaque, "Saque", conta->get_titular()->get_nome());
+            transacoes.push_back(t);
+            }
+            else
+            {
+                std::cout << "====================================================================\n";
+                throw ExcecaoSaque();
+                std::cout << "====================================================================\n";
+            }
+        }
+    }
+
+    else if (opcao == 2)
+    {
+            float valorDeposito;
+            std::cout << "====================================================================\n";
+            std::cout << "Digite o valor do depósito ou digite 9 para cancelar a operação" << std::endl;
+            std::cout << "↓\n";
+            std::cin >> valorDeposito;
+            system("clear");
+                if (valorDeposito == 9){
+                    system("clear");
+                    Banco::selecionaOperacao();
+                }
+            else
+            {
+            conta->realizaDeposito(valorDeposito);
+            Transacao t(valorDeposito, "Depósito", conta->get_titular()->get_nome());
+            transacoes.push_back(t);
+        }
+    }
+    else if (opcao == 3)
+    {
+            float valorTransferencia;
+            bool usuarioExiste =  false;
+            std::string userReceber;
+            std::cout << "\nInforme o valor a ser transferido e o usuário de quem vai receber ou digite 9 para cancelar a operação: " << std::endl;
+            std::cout << "↓\n";
+            std::cin >> valorTransferencia >> userReceber;
+            system("clear");
+            if (valorTransferencia == 9){
+                    system("clear");
+                Banco::selecionaOperacao();
+                }
+            else if (valorTransferencia != 9) {
+                while(!usuarioExiste){
+                    if(existe(userReceber,Users)){
+                            usuarioExiste = true;
+                }
+
+                    else {
+                                    usuarioExiste = false;
+                        std::cout << "====================================================================\n";
+                                    std::cout << "Digite um usuário válido.\n";
+                        std::cout << "====================================================================\n";
+                                }
+
+            conta-> transfere(userReceber, valorTransferencia, Contas);
+           Transacao t(valorTransferencia, "Transferencia", conta->get_titular()->get_nome(), Contas[userReceber]->get_titular()->get_nome());
+            transacoes.push_back(t);
+            }
+        }
+    }
+    else if (opcao == 4)
+    {
+        conta->imprimeSaldo();
+    }
+    else if (opcao == 5)
+    {
+        std::cout << "====================================================================\n";
+    }
+    else if (opcao == 6)
+    {
+        if (transacoes.size() == 0) {
+            system("clear");
+            std::cout << "====================================================================\n";
+            std::cout << "Nenhuma transação foi realizada!\n";
+            std::cout << "====================================================================\n";
+        }
+        else if (transacoes.size() != 0) {
+        std::cout << "====================================================================\n";
+        std::cout << "Extrato: \n\n";
+            imprimeExtrato();
+        std::cout << "====================================================================\n";
+        }
+    }
+    else if (opcao == 7)
+    {
+        std::cout << "\n============ Agradecemos por utilizar o NossoBanco! ================" << std::endl;
+        std::cout << "====================================================================\n";
+    }
+}
