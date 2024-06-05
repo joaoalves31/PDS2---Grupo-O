3c4e8dd61aad8cb1d1c2e37b287ff7fbb028e525
andreassisrg <andreassisrg@gmail.com>
2024-06-05 20:11:45 -0300

    feature: adição contrato entidade Banco

--- include/banco.hpp
+++ include/banco.hpp
@@ -1,1 +1,38 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "conta.hpp"
+#include <map>
+#include "transacao.hpp"
+
+
+class Banco
+{
+public:
+
+    std::vector<std::string> Users;
+
+    std::map<std::string, Conta *> Contas;
+
+    std::vector<Transacao>transacoes;
+
+    float saldo;
+
+public:
+
+    Banco();
+
+    void adicionaConta(Conta &conta);
+
+    Conta *login();
+
+    int selecionaOperacao();
+
+    void realizaOperacao(int opcao, Conta *conta);
+
+    float get_saldo();
+
+    void imprimeExtrato();
+};
