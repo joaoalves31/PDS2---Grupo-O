b0dc390820d72ee559df13772562694c5861aa36
waramaral <wara.amaral@outlook.com>
2024-06-06 10:39:11 -0300

    Update titular.hpp

---
+++ include/titular.hpp
@@ -0,0 +1,23 @@
+#ifndef __TITULAR_HPP__
+#define __TITULAR_HPP__
+
+#include <iostream>
+#include <string>
+#include "pessoa.hpp"
+
+class Titular : public Pessoa
+{
+private:
+
+    std::string usuario, senha;
+
+public:
+
+    Titular(std::string n, int i, std::string c, std::string u, std::string s);
+    Titular(std::string n, int i, std::string c);
+ 
+    std::string get_senha();
+    std::string get_usuario();
+};
+
+#endif
