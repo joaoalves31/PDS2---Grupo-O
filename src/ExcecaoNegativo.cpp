70be38f7963d3581f8353782c86af6db04aed6b6
waramaral <wara.amaral@outlook.com>
2024-06-06 16:06:49 -0300

    Update ExcecaoNegativo.cpp

--- src/ExcecaoNegativo.cpp
+++ src/ExcecaoNegativo.cpp
@@ -3,3 +3,7 @@
 ExcecaoNegativo::ExcecaoNegativo(){
     _erro="Impossível realizar operações com valores negativos";
 }
+
+const char* ExcecaoNegativo::what() const noexcept{
+  return _erro.c_str();
+}
