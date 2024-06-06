aa90dc41add0de169961d1555b4e6024961a3365
waramaral <wara.amaral@outlook.com>
2024-06-06 10:40:46 -0300

    Update titular.cpp

---
+++ src/titular.cpp
@@ -0,0 +1,25 @@
+#include "titular.hpp"
+
+
+Titular::Titular(std::string n, int i, std::string c, std::string u, std::string s) : Pessoa(n, i, c)
+{
+        usuario = u;
+        senha = s;
+
+        std::cout << get_nome() << " foi registrado no banco.\n" << std::endl;
+}
+
+
+Titular::Titular(std::string n, int i, std::string c) : Pessoa(n, i, c) {}
+
+
+std::string Titular::get_senha()
+{
+        return senha;
+}
+
+
+std::string Titular::get_usuario()
+{
+        return usuario;
+}
